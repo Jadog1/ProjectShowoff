997812b51a89374d815a8c63cf7ec19285f056a0
Jadog1 <steijr03@pfw.edu>
2021-03-02 15:46:11 -0500

    Added image distort

--- ProjectShowoff/server.js
+++ ProjectShowoff/server.js
@@ -19,6 +19,13 @@ http.createServer(function (req, res) {
                 return res.end();
             });
             break;
+        case '/distort':
+            fs.readFile('ImageDistort.html', function (err, data) {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(data);
+                return res.end();
+            });
+            break;
         case '/distance':
             fs.readFile('Distance.html', function (err, data) {
                 res.writeHead(200, { 'Content-Type': 'text/html' });
